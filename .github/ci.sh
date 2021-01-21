05a09ac09b7e14fa1db291af4d138b6cf0026910
Aaron Tomb <atomb@galois.com>
2021-01-21 15:55:53 -0800

    Don't build saw-remote-api on Windows for now

--- .github/ci.sh
+++ .github/ci.sh
@@ -39,13 +39,13 @@ retry() {
 
 setup_dist_bins() {
   if $IS_WIN; then
-    is_exe "dist/bin" "saw" && is_exe "dist/bin" "saw-remote-api" && return
+    is_exe "dist/bin" "saw" && return
   else
     is_exe "dist/bin" "saw" && is_exe "dist/bin" "saw-remote-api" && is_exe "dist/bin" "jss" && return
     extract_exe "jss" "dist/bin"
+    extract_exe "saw-remote-api" "dist/bin"
   fi
   extract_exe "saw" "dist/bin"
-  extract_exe "saw-remote-api" "dist/bin"
   export PATH=$PWD/dist/bin:$PATH
   echo "$PWD/dist/bin" >> "$GITHUB_PATH"
   strip dist/bin/saw* || echo "Strip failed: Ignoring harmless error"
@@ -123,12 +123,12 @@ build() {
   cp cabal.GHC-"$ghc_ver".config cabal.project.freeze
   cabal v2-update
   echo "allow-newer: all" >> cabal.project.local
-  pkgs=(saw saw-remote-api)
+  pkgs=(saw)
   if $IS_WIN; then
     echo "flags: -builtin-abc" >> cabal.project.local
     echo "constraints: jvm-verifier -builtin-abc, cryptol-saw-core -build-css" >> cabal.project.local
   else
-    pkgs+=(jss)
+    pkgs+=(jss saw-remote-api)
   fi
   echo "allow-newer: all" >> cabal.project.local
   tee -a cabal.project > /dev/null < cabal.project.ci
